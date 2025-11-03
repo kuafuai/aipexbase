00aaa848b51e8fa77d896a1b2a0766c99063e63c
Yake <jiangfei@kuafuai.net>
2025-11-03 19:09:57 +0800

    feat(i18n): tables add/export i18n

--- frontend/src/views/project/tables.vue
+++ frontend/src/views/project/tables.vue
@@ -194,7 +194,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"/>
               </svg>
-              导出数据
+              {{ t('page.tables.export_data') }}
             </el-button>
             <el-button
                 type="primary"
@@ -205,7 +205,7 @@
               <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
               </svg>
-              添加数据
+              {{ t('page.tables.add_data') }}
             </el-button>
           </div>
 
