ef04ae405e42f0503a38793a03e6d614d7abbd1a
harry03612 <sunhao@kuafuai.net>
2025-12-29 14:30:06 +0800

    恢复ApplicationConfig

--- backend/src/main/java/com/kuafuai/config/ApplicationConfig.java
+++ backend/src/main/java/com/kuafuai/config/ApplicationConfig.java
@@ -1,18 +1,28 @@
 package com.kuafuai.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 @Configuration
 public class ApplicationConfig {
 
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     @Bean
-    public ObjectMapper objectMapper() {
-        return new Jackson2ObjectMapperBuilder()
-                .modules(new JavaTimeModule())
-                .build();
+    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
+        return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder.timeZone(TimeZone.getDefault());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
+            jacksonObjectMapperBuilder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(formatter));
+            jacksonObjectMapperBuilder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(formatter));
+        };
     }
 }
