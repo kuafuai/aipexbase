16e38c9ec2054361611b117f2bec4b7a437453e3
kuafu-limanxiang <limanxiang@kuafuai.net>
2026-02-03 16:17:23 +0800

    feat:支付相关接口，支持多数据源

--- backend/src/main/java/com/kuafuai/pay/controller/GeneralOrderBusinessController.java
+++ backend/src/main/java/com/kuafuai/pay/controller/GeneralOrderBusinessController.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Lists;
 import com.kuafuai.common.domin.BaseResponse;
 import com.kuafuai.common.domin.ResultUtils;
 import com.kuafuai.common.dynamic_config.ConfigContext;
+import com.kuafuai.config.db.DatabaseRouterAspect;
+import com.kuafuai.config.db.DynamicDataSourceContextHolder;
 import com.kuafuai.login.handle.GlobalAppIdFilter;
 import com.kuafuai.pay.business.OrderFacadeService;
 import com.kuafuai.pay.config.WxV3PayConfig;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/generalOrder")
@@ -29,45 +32,79 @@ public class GeneralOrderBusinessController {
      */
     @PostMapping("/{operateName}")
     public BaseResponse<?> handleOrder(@PathVariable String operateName, @RequestBody Map<String, Object> body) {
-        return orderFacadeService.handleOrder(GlobalAppIdFilter.getAppId(), operateName, body);
+        final String appId = GlobalAppIdFilter.getAppId();
+        return executeWithDataSource(appId,() ->
+                orderFacadeService.handleOrder(GlobalAppIdFilter.getAppId(), operateName, body)
+        );
+
     }
 
     @GetMapping("/payMethod")
 
     public BaseResponse<?> payMethod() {
-        final WxV3PayConfig wxV3PayConfig = WxV3PayConfig.builder()
-                .appId(ConfigContext.getDatabase())
-                .build();
-        final ArrayList<String> strings = Lists.newArrayList();
-        final Boolean wxEnable = wxV3PayConfig.getWxEnable();
-        final Boolean mockEnable = wxV3PayConfig.getMockEnable();
-        final Boolean stripeEnable = wxV3PayConfig.getStripeEnable();
+        final String appId = ConfigContext.getDatabase();
+        return executeWithDataSource(appId,()->{
+            final WxV3PayConfig wxV3PayConfig = WxV3PayConfig.builder()
+                    .appId(appId)
+                    .build();
+            final ArrayList<String> strings = Lists.newArrayList();
+            final Boolean wxEnable = wxV3PayConfig.getWxEnable();
+            final Boolean mockEnable = wxV3PayConfig.getMockEnable();
+            final Boolean stripeEnable = wxV3PayConfig.getStripeEnable();
 
-        if (wxEnable) {
-            strings.add("wechat");
-        }
+            if (wxEnable) {
+                strings.add("wechat");
+            }
 
-        if (mockEnable) {
-            strings.add("mock");
-        }
+            if (mockEnable) {
+                strings.add("mock");
+            }
 
-        if (stripeEnable) {
-            strings.add("stripe");
-        }
+            if (stripeEnable) {
+                strings.add("stripe");
+            }
 
-        return ResultUtils.success(strings);
-
+            return ResultUtils.success(strings);
+        });
     }
 
 
     @PostMapping("/callback/{database}/{payChannel}")
-    public Object paySuccessCallback(@PathVariable String database,@PathVariable String payChannel,@RequestBody String requestData, @RequestHeader Map<String, String> headers) {
+    public Object paySuccessCallback(@PathVariable String database, @PathVariable String payChannel, @RequestBody String requestData, @RequestHeader Map<String, String> headers) {
+        return executeWithDataSource(database,()->{
+            try {
+                ConfigContext.setDatabase(database);
+                log.info("paySuccessCallback payChannel:{},requestData:{},headers:{}", payChannel, requestData, headers);
+                return orderFacadeService.handleOrderCallback(payChannel, requestData, headers, database);
+            } finally {
+                ConfigContext.clear();
+            }
+        });
+    }
+
+
+
+    private  <T> T executeWithDataSource(String appId,Supplier<T> supplier) {
+
+        final String rdsKey = DatabaseRouterAspect.getOrAllocateRdsKey(appId, "app");
+
         try {
-            ConfigContext.setDatabase(database);
-            log.info("paySuccessCallback payChannel:{},requestData:{},headers:{}", payChannel, requestData, headers);
-            return orderFacadeService.handleOrderCallback(payChannel,requestData,headers,database);
-        }finally {
-            ConfigContext.clear();
+            DynamicDataSourceContextHolder.setDataSourceType(rdsKey);
+            return supplier.get();
+        } finally {
+            DynamicDataSourceContextHolder.clearDataSourceType();
+        }
+    }
+
+    private void executeWithDataSource(String appId,Runnable runnable) {
+
+        final String rdsKey = DatabaseRouterAspect.getOrAllocateRdsKey(appId, "app");
+
+        try {
+            DynamicDataSourceContextHolder.setDataSourceType(rdsKey);
+            runnable.run();
+        } finally {
+            DynamicDataSourceContextHolder.clearDataSourceType();
         }
     }
 }
