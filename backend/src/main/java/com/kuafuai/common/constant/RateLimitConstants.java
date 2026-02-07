318f699fbd43708cd6af3515a47077554ddceb11
Yake <jiangfei@kuafuai.net>
2026-02-07 17:09:16 +0800

    feat(data): 限流每分钟限制150个请求

--- backend/src/main/java/com/kuafuai/common/constant/RateLimitConstants.java
+++ backend/src/main/java/com/kuafuai/common/constant/RateLimitConstants.java
@@ -12,12 +12,12 @@ public class RateLimitConstants {
         /**
          * 每秒请求数限制
          */
-        public static final double REQUESTS_PER_SECOND = 20.0;
+        public static final double REQUESTS_PER_SECOND = 50.0;
 
         /**
          * 每分钟请求数限制
          */
-        public static final double REQUESTS_PER_MINUTE = 100.0;
+        public static final double REQUESTS_PER_MINUTE = 150.0;
 
         /**
          * 数据清理间隔时间（秒）
